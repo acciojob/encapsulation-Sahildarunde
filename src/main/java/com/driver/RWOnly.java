d08546ea3f997275e86055fcc92a29442460e73d
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-01 18:14:18 +0530

    getters & setters added

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public String setName(String name){
+        this.name = name;
+        return name;
+    }
 }
